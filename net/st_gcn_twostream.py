268c0f64bcf252bccf01fe3fc6dd5ac104781363
littlepure2333 <rz8217525@163.com>
2020-04-17 22:52:40 +0800

    modify two stream

--- net/st_gcn_twostream.py
+++ net/st_gcn_twostream.py
@@ -18,9 +18,11 @@ class Model(nn.Module):
 
     def forward(self, x):
         N, C, T, V, M = x.size()
+        # m = torch.cat((torch.cuda.FloatTensor(N, C, 1, V, M).zero_(),
+        #                 x[:, :, 1:-1] - 0.5 * x[:, :, 2:] - 0.5 * x[:, :, :-2],
+        #                 torch.cuda.FloatTensor(N, C, 1, V, M).zero_()), 2)
         m = torch.cat((torch.cuda.FloatTensor(N, C, 1, V, M).zero_(),
-                        x[:, :, 1:-1] - 0.5 * x[:, :, 2:] - 0.5 * x[:, :, :-2],
-                        torch.cuda.FloatTensor(N, C, 1, V, M).zero_()), 2)
+                        x[:, :, 1:] - x[:, :, :-1]), 2)
 
         res = self.origin_stream(x) + self.motion_stream(m)
         return res
